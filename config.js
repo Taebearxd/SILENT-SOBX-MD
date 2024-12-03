5c2cc250c59e16096a3f624279a8df9ed2c48028
Taebearxd <149920040+Taebearxd@users.noreply.github.com>
2024-12-03 17:56:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "edwxGb6b#5DKreSiU4sHZTlPXS-b79iP3orIZfKOXbAZNTL-b8JU",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
     MODE: process.env.MODE || "public",
